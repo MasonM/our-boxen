2aa650b2079cf2f6b6e411033732a8bb76616e07
Mason Malone <mason@masonm.org>
2013-08-29 09:56:45 -0400

    Install FileZilla

--- manifests/site.pp
+++ manifests/site.pp
@@ -62,6 +62,12 @@ node default {
   include adium
   include thunderbird
 
+  package { 'FileZilla':
+    ensure   => installed,
+    source   => 'http://downloads.sourceforge.net/project/filezilla/FileZilla_Client/3.7.3/FileZilla_3.7.3_i686-apple-darwin9.app.tar.bz2',
+    provider => 'compressed_app',
+  }
+
   # common, useful packages
   package {
     [
